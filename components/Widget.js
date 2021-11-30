85853469a2c7c1e7e96287a42900e9db3ed599c4
Laxman Desai <lolopaw24@gmail.com>
2021-12-01 04:29:20 +0530

    🐛 FIX: Use jsx as file extension

--- components/Widget.js
+++
@@ -1,9 +0,0 @@
-function Widget() {
-    return (
-        <div className="text-textColor relative w-full p-2 h-56 rounded-xl bg-secondary">
-            Hi! I'm a Widget
-        </div>
-    )
-}
-
-export default Widget
